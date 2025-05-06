fd09b99e477d25844fbebaecdd86142040527b0e
Logan Helstad <helstadl@oregonstate.edu>
2025-05-05 17:54:53 -0700

    changed menu css

--- web/restaurant-example/src/pages/Menu.jsx
+++ web/restaurant-example/src/pages/Menu.jsx
@@ -1,24 +1,27 @@
 import { Worker, Viewer } from '@react-pdf-viewer/core';
 import { zoomPlugin } from '@react-pdf-viewer/zoom';
 import '@react-pdf-viewer/core/lib/styles/index.css';
+import './../Pdf.css'; // Add this line to import the custom CSS override
 
 function Menu() {
   const zoomPluginInstance = zoomPlugin();
-  const { zoomTo } = zoomPluginInstance;
 
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Menu</h1>
+    <div style={{
+      padding: '2rem',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+    
 
-      <div style={{ height: "80vh", maxWidth: "630px", border: "1px solid rgba(0, 0, 0, 0.3)" }}>
+      <div className="pdf-container">
         <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
           <Viewer
             fileUrl="/Cocktail Party Sample Menu.pdf"
             plugins={[zoomPluginInstance]}
-            onDocumentLoad={() => {
-            
-              zoomTo(0.75);
-            }}
+            defaultScale={0.75}
           />
         </Worker>
       </div>
