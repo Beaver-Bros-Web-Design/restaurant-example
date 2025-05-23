ede5412cbd74a66d9639780f15e6cd753c6a8349
Logan Helstad <helstadl@oregonstate.edu>
2025-05-22 17:17:19 -0700

    quick ui update

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from app.config import Config
 from app.routes.catering import create_catering_blueprint
 from .routes.menu import create_menu_blueprint
+from .routes.auth import create_auth_blueprint
 from flask_cors import CORS
 import os
 
@@ -23,10 +24,13 @@ def create_app():
    
 
     catering_collection = db_service.get_collection_service(os.getenv("CATERING_COLLECTION_NAME"))
-
+    user_collection = db_service.get_collection_service(os.getenv("USER_COLLECTION_NAME"))
     # main_bp = create_menu_blueprint(menu_collection)
 
     main_bp = create_catering_blueprint(catering_collection)
+    auth_bp = create_auth_blueprint(user_collection)
+
     app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
 
     return app
--- server/app/routes/__init__.py
+++ server/app/routes/__init__.py
@@ -1,2 +1,3 @@
 from .menu import create_menu_blueprint
 from .catering import create_catering_blueprint
+from .auth import create_auth_blueprint
